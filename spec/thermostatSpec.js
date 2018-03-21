d7f8543271f6ab6a32cd3eb764bdf2abe9d030b5
Charlene Bastians <charlenebastians@hotmail.com>
2018-03-21 16:57:54 +0000

    up, down, psm

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -8,7 +8,39 @@ describe('Thermostat', function() {
   });
 
   it('starts at 20 degrees', function() {
-    expect(thermostat.temperature).toEqual(20)
+    expect(thermostat.currentTemp()).toEqual(20)
   });
 
+  it('increases in temperature with up()', function() {
+    thermostat.up();
+    expect(thermostat.currentTemp()).toEqual(21);
+  });
+
+
+  it('increases in temperature with down()', function() {
+    thermostat.down();
+    expect(thermostat.currentTemp()).toEqual(19)
+  });
+
+  it('has a minimum of 10 degrees', function() {
+    for (var i = 0; i < 11; i++) {
+      thermostat.down();
+    }
+    expect(thermostat.currentTemp()).toEqual(10);
+  });
+
+  it('has a power saving mode on by default', function() {
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  });
+
+  it('can switch power saving mode off', function() {
+    thermostat.switchPowerSavingModeOff();
+    expect(thermostat.isPowerSavingModeOn()).toBe(false);
+  });
+
+  it('power mode can switch back on', function () {
+    thermostat.switchPowerSavingModeOff();
+    thermostat.switchPowerSavingModeOn();
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -2,4 +2,37 @@
 
 function Thermostat() {
   this.temperature = 20
+  this.MINIMUM_TEMPERATURE = 10
+  this.powerSavingMode = true;
+}
+
+Thermostat.prototype.currentTemp = function() {
+  return this.temperature;
+}
+
+Thermostat.prototype.up = function() {
+  this.temperature ++;
+};
+
+Thermostat.prototype.down = function() {
+  if (this.isMinimumTemperature()) {
+    return;
+  }
+  this.temperature --;
+}
+
+Thermostat.prototype.isMinimumTemperature = function() {
+  return this.temperature === this.MINIMUM_TEMPERATURE
+}
+
+Thermostat.prototype.isPowerSavingModeOn = function () {
+  return this.powerSavingMode === true;
+}
+
+Thermostat.prototype.switchPowerSavingModeOff = function() {
+  this.powerSavingMode = false;
+}
+
+Thermostat.prototype.switchPowerSavingModeOn = function() {
+  this.powerSavingMode = true;
 }
