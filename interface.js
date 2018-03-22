486d26839854fd0a59b066040b6001ff598c4d43
Charlene Bastians <charlenebastians@hotmail.com>
2018-03-22 17:18:25 +0000

    select city function added

--- interface.js
+++ interface.js
@@ -32,13 +32,19 @@ $(document).ready(function() {
     $('#temperature').attr('class', thermostat.energyUsage());
   };
 
-  $('#current-city').change(function() {
+  displayWeather('London');
+
+  $('#select-city').submit(function(event) {
+    event.preventDefault();
     var city = $('#current-city').val();
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
-      $('#current-temperature').text(data.main.temp)
-    })
+    displayWeather(city);
   })
-
-
-
+  function displayWeather(city) {
+   var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+   var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+   var units = '&units=metric';
+   $.get(url + token + units, function(data) {
+     $('#outside-temperature').text(data.main.temp);
+  });
+ }
 });
