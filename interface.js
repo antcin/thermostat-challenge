781e018f96833908c164448be94d9b7f0651c2cb
Charlene Bastians <charlenebastians@hotmail.com>
2018-03-22 15:33:35 +0000

    temp up and down view

--- interface.js
+++ interface.js
@@ -1,4 +1,16 @@
-$(document).ready(function({
+$(document).ready(function() {
   var thermostat = new Thermostat();
   $('#temperature').text(thermostat.temperature);
-}))
+
+  $('#temperature-up').on('click', function() {
+    thermostat.up();
+    $('#temperature').text(thermostat.temperature);
+  })
+
+  $('#temperature-down').click(function() {
+    thermostat.down();
+    $('#temperature').text(thermostat.temperature);
+  })
+
+
+})
