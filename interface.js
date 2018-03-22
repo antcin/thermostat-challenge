95c97abe8bc5128eb1e3c19538fe555c1cab365f
antcin <ciniglioantonio@gmail.com>
2018-03-22 15:46:34 +0000

    Hook interface buttons up

--- interface.js
+++ interface.js
@@ -4,17 +4,30 @@ $(document).ready(function() {
 
   $('#temperature-up').click(function() {
     thermostat.up();
-    updateTemp()
-  })
+    updateTemp();
+  });
 
   $('#temperature-down').click(function() {
     thermostat.down();
-    updateTemp()
-  })
+    updateTemp();
+  });
+
+  $('#temperature-reset').click(function(){
+    thermostat.resetTemperature();
+    updateTemp();
+  });
+
+  $('#powersaving-on').click(function(){
+    thermostat.switchPowerSavingModeOn();
+    $('#power_saving_status').text('on');
+  });
+
+  $('#powersaving-off').click(function(){
+    thermostat.switchPowerSavingModeOff();
+    $('#power_saving_status').text('off');
+  });
 
   function updateTemp() {
     $('#temperature').text(thermostat.temperature);
-  }
-
-
-})
+  };
+});
